3201c285b2f46494873be29678742a226b3bffa0
GustavoTxFreitas <62919596+GustavoTxFreitas@users.noreply.github.com>
2020-08-15 23:46:29 -0300

    Alteração de Design

--- src/javascript/script.js
+++ src/javascript/script.js
@@ -1,13 +1,18 @@
 function Bin2Dec() {
+
     var dec = ok = i = 0;
-    var bin = document.getElementById("input").value;
-    for (i; i < bin.length; i++) {
-        if (bin[i] != "0" && bin[i] != "1") {
+    var bin = document.getElementById("input").value.trim();
+
+    for (i; i < bin.length; i++) 
+    {
+        if (bin[i] != "0" && bin[i] != "1")
+        {
             ok = 1;
             break;
         }
         dec = dec * 2 + parseInt(bin[i]);
     }
+
     document.getElementById("display")
         .innerText = ok == 0 ? dec : "non-binary";
 }
